866eff062c1e3151a324d6620be5d1b81d7e32c6
nathanfaucett <nathanfaucett@gmail.com>
2015-01-16 12:46:22 -0600

    clean up

--- test/test.js
+++ test/test.js
@@ -4,14 +4,14 @@ var assert = require("assert"),
 
 describe("bind(fn, thisArg, ...args)", function() {
     it("should bind thisArg and args to passed function", function() {
-        function test(a) {
-            return this.name + " age: " + a;
+        function test(a, b) {
+            return this.name + " age: " + a + " last: " + b;
         }
 
         bound = bind(test, {
             name: "Bob"
         }, 32);
 
-        assert.equal(bound("Bobby"), "Bob age: 32");
+        assert.equal(bound("Bobby"), "Bob age: 32 last: Bobby");
     });
 });
