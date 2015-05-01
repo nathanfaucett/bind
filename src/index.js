7f18886f5a5b1c046b23c46de4d0bbb95ad46628
Nathan Faucett <nathanfaucett@gmail.com>
2015-05-01 11:07:33 -0500

    updates

--- src/index.js
+++ src/index.js
@@ -3,6 +3,16 @@ var isFunction = require("is_function"),
     createFunctionWrapper = require("create_function_wrapper");
 
 
+module.exports = bind;
+
+
+function bind(fn, thisArg) {
+    if (!isFunction(fn)) {
+        throw new TypeError("bind(fn, thisArg, ...args) fn must be a function");
+    }
+    return baseBind(fn, thisArg, arguments.length > 2 ? fastSlice(arguments, 2) : null);
+}
+
 function baseBind(fn, thisArg, args) {
     var bound = createFunctionWrapper(fn),
         wrapper = bound.__wrapper__;
@@ -15,10 +25,3 @@ function baseBind(fn, thisArg, args) {
 
     return bound;
 }
-
-module.exports = function bind(fn, thisArg) {
-    if (!isFunction(fn)) {
-        throw new TypeError("bind(fn, thisArg, ...args) fn must be a function");
-    }
-    return baseBind(fn, thisArg, arguments.length > 2 ? fastSlice(arguments, 2) : null);
-};
